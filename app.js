718ee543bf41712583b63583b0ab4a9d25ffaad3
King LEE <a0988698776@gmail.com>
2020-11-12 10:18:46 +0800

    fetch: expense-tracker init

---
+++ app.js
@@ -0,0 +1,26 @@
+// require packages used in the project
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const port = 3000
+
+const app = express()
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use((bodyParser.urlencoded({ extended: true })))
+app.use(methodOverride('_method'))
+
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
