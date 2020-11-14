5a51b59357f41b06b2a996f0c7c8ac12a1a55d2e
King LEE <a0988698776@gmail.com>
2020-11-14 16:14:56 +0800

    feat: add user authentication in todo and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  authenticator: (req, res, next) => { // 會出一個物件 叫做authenticator 物件的 的函式
+    if (req.isAuthenticated()) { // Passport.js 提供的函式，會根據 request 的登入狀態回傳 true 或 false。
+      return next()
+    }
+    res.redirect('/users/login')
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const record = require('./modules/record')
-const users = require('./modules/users') // add this
+const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/record', record)
+router.use('/record', authenticator, record)
 router.use('/users', users)
+router.use('/', authenticator, home)
+
 module.exports = router
