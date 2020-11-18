502c93f4149999c72a3b5d1bb5de876e5c790d69
King LEE <a0988698776@gmail.com>
2020-11-18 15:33:34 +0800

    refactor: seed data

--- models/category.js
+++ models/category.js
@@ -10,11 +10,7 @@ const categorySchema = new Schema({
   icon: {
     type: String,
     default: '<i class="far fa-user fa-2x"></i>'
-  },
-  records: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Record'
-  }]
+  }
 })
 
 module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -7,6 +7,8 @@ const Category = require('../category')
 const db = require('../../config/mongoose')
 const bcrypt = require('bcryptjs')
 
+const recordsExpense = require('../seeds/data/records.json')
+
 const SEED_USER = {
   username: '廣志',
   email: 'user@root.com',
@@ -15,62 +17,30 @@ const SEED_USER = {
 
 db.once('open', () => {
   console.log('MongoDB connected recordSeeder!')
-  const createUserPromises = []
 
-  const { username, email } = SEED_USER
-  createUserPromises.push(
-    User.find({ email: SEED_USER.email })
-      .then(user => {
-        if (user.length !== 0) {
-          return
-        }
-        return bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(SEED_USER.password, salt))
-          .then(hash => {
-            return User.create({ username, email, password: hash })
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({ username: SEED_USER.username, email: SEED_USER.email, password: hash }))
+    .then(userId => {
+      return Promise.all(Array.from(
+        { length: recordsExpense.records.length },
+        (_, i) => Category
+          .findOne({ name: recordsExpense.records[i].category })
+          .then(category => Record.create({
+            userId,
+            name: recordsExpense.records[i].name,
+            category: category._id,
+            date: new Date(recordsExpense.records[i].date),
+            amount: recordsExpense.records[i].amount,
+            merchant: recordsExpense.records[i].merchant
           })
-      })
-  )
-  Promise.all(createUserPromises).then(() => {
-    const createRecordPromise = []
-    createRecordPromise.push(
-      User.findOne({ email })
-        .then(user => {
-          const userId = user._id
-          return Record.create(
-            {
-              name: '早餐',
-              category: '餐飲食品',
-              date: Date.now(),
-              amount: 120,
-              merchant: '全家便利商店',
-              icon: '<i class="fas fa-utensils"></i>',
-              userId
-            },
-            {
-              name: '午餐',
-              category: '餐飲食品',
-              date: Date.now(),
-              amount: 300,
-              merchant: '麥當勞',
-              icon: '<i class="fas fa-utensils"></i>',
-              userId
-            },
-            {
-              name: '電影',
-              category: '休閒娛樂',
-              date: Date.now(),
-              amount: 250,
-              merchant: '威尼斯影城',
-              icon: '<i class="fas fa-grin-beam"></i>',
-              userId
-            }
           )
-        })
-    )
-    Promise.all(createRecordPromise).then(item => {
+      ))
+    })
+    .then(() => {
+      console.log('Done')
+      // process.exit() 指「關閉這段 Node 執行程序」
       process.exit()
     })
-  })
 })
